74aab162cfba8b48be46e4c0c85109e6d0efaec1
FithaAqyla <fithaqila@gmail.com>
2023-04-03 08:04:37 +0700

    ganti elevatedbttn

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -9,100 +9,99 @@ class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-        resizeToAvoidBottomInset: false,
-        body: Container(
-          child: Center(
-              child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Form(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Text(
-                                  "Pokedex.",
-                                  style: TextStyle(
-                                      fontSize: 34,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color.fromARGB(255, 255, 175, 5)),
-                                ),
-                              ]),
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        child: Center(
+            child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Form(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                "Pokedex.",
+                                style: TextStyle(
+                                    fontSize: 34,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(255, 255, 175, 5)),
+                              ),
+                            ]),
+                      ),
+                      SizedBox(
+                        height: 60,
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                "Welcome to Pokedex",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey),
+                              ),
+                            ]),
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                "You can find your favorite pokemon here",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey),
+                              ),
+                            ]),
+                      ),
+                      SizedBox(
+                        height: 100,
+                      ),
+                      Image.asset(
+                        'assets/image/pokeball.png',
+                        width: 180,
+                        height: 150,
+                      ),
+                      SizedBox(
+                        height: 100,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginPage()));
+                        },
+                        child: Icon(
+                          Icons.navigate_next,
+                          color: Colors.white,
                         ),
-                        SizedBox(
-                          height: 60,
-                        ),
-                        SizedBox(
-                          height: 30.0,
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Text(
-                                  "Welcome to Pokedex",
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.grey),
-                                ),
-                              ]),
-                        ),
-                        SizedBox(
-                          height: 30.0,
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Text(
-                                  "You can find your favorite pokemon here",
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.grey),
-                                ),
-                              ]),
-                        ),
-                        SizedBox(
-                          height: 100,
-                        ),
-                        Image.asset(
-                          'assets/image/pokeball.png',
-                          width: 180,
-                          height: 150,
-                        ),
-                        SizedBox(
-                          height: 100,
-                        ),
-                        // ElevatedButton(
-                        //   onPressed: () {
-                        //     Navigator.push(
-                        //         context,
-                        //         MaterialPageRoute(
-                        //             builder: (context) => LoginPage()));
-                        //   },
-                        //   child: Icon(
-                        //     Icons.navigate_next,
-                        //     color: Colors.white,
-                        //   ),
-                        //   style: ElevatedButton.styleFrom(
-
-                        //       primary: Color.fromARGB(255, 255, 175, 55),
-                        //       shape: RoundedRectangleBorder(
-                        //           borderRadius: BorderRadius.circular(50.0))),
-                        // )
-                      ],
-                    ),
-                  ))),
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => LoginPage()));
-            },
-            child: Icon(
-              Icons.navigate_next,
-              color: Colors.white,
-            )));
+                        style: ElevatedButton.styleFrom(
+                            primary: Color.fromARGB(255, 255, 175, 55),
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(20)),
+                      )
+                    ],
+                  ),
+                ))),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //     onPressed: () {
+      //       Navigator.push(context,
+      //           MaterialPageRoute(builder: (context) => LoginPage()));
+      //     },
+      //     child: Icon(
+      //       Icons.navigate_next,
+      //       color: Colors.white,
+      //     ))
+    );
   }
 }
